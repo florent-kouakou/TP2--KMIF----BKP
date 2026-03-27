5a4edc49ce0f78e68e4df443689d2b575309a95e
parfaitbombomalapin <kangbenibomboma@gmail.com>
2026-03-27 15:43:14 -0400

    get set pays

--- TP2-KMIF -- BKP/src/livres/Pays.java
+++ TP2-KMIF -- BKP/src/livres/Pays.java
@@ -21,6 +21,22 @@ public class Pays {
         }
     }
 
+    public String getCodePays() {
+        return codePays;
+    }
+
+    public void setCodePays(String codePays) {
+        this.codePays = codePays;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
     @Override
     public String toString() {
         return "Pays{" +
