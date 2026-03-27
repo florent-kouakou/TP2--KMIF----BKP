0f48d01e0c34e59f47445dc5c79e74233792252f
parfaitbombomalapin <kangbenibomboma@gmail.com>
2026-03-27 16:46:29 -0400

    reecriture sous classes

---
+++ TP2-KMIF -- BKP/src/livres/OuvragePapier.java
@@ -0,0 +1,7 @@
+package livres;
+
+public class OuvragePapier extends Ouvrage{
+    public OuvragePapier(String titre, Auteur auteur) {
+        super(titre, auteur);
+    }
+}
---
+++ TP2-KMIF -- BKP/src/livres/OuvrageVideo.java
@@ -0,0 +1,7 @@
+package livres;
+
+public class OuvrageVideo extends Ouvrage{
+    public OuvrageVideo(String titre, Auteur auteur) {
+        super(titre, auteur);
+    }
+}
